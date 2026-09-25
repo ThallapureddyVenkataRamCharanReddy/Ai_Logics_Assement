82be584b37adc724ae0ebc7a7f21f89caf787a53
ThallapureddyVenkataRamCharanReddy <tvenkataramcharanreddy@gmail.com>
2026-09-25 13:03:22 +0530

    Update problem1.py
    Refactor code to remove unnecessary whitespace and comments.

--- problem1.py
+++ problem1.py
@@ -1,16 +1,16 @@
-n = int(input())
-slots = [list(map(int, input().split())) for _ in range(n)]
+n=int(input())
+slots=[list(map(int, input().split())) for _ in range(n)]
 # print(slots)
 slots.sort()
 #i will sort based on start position
-start, end = slots[0]
+start,end=slots[0]
 #forloop is starting from 1st index ,because we will iterate from 1st position
-for s, e in slots[1:]:
-    if s <= end:  #If the ranges overlap, merge them
-        end = max(end, e)
+for s,e in slots[1:]:
+    if s<=end:  #If the ranges overlap, merge them
+        end=max(end, e)
     else:# If they don't overlap, print the previous range
         print(start, end)
       #update the start and end
-        start, end = s, e
-print(start, end)
+        start,end=s,e
+print(start,end)
 #this is because , the  last range doesn't get printed inside the for loop
