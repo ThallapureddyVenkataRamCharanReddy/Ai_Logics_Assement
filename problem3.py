ba20da407054cd015d559cfdf9b41ddd86ca6ed0
ThallapureddyVenkataRamCharanReddy <tvenkataramcharanreddy@gmail.com>
2026-09-25 13:06:20 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,20 +1,20 @@
-n = int(input())
-a = list(map(int, input().split()))
+n=int(input())
+a=list(map(int, input().split()))
 
-m = int(input())
-b = list(map(int, input().split()))
+m=int(input())
+b=list(map(int, input().split()))
 
-result = []
-carry = 0
+result=[]
+carry=0
 
 for i in range(max(n, m)):
-    x = a[i] if i < n else 0
-    y = b[i] if i < m else 0
+    x=a[i] if i < n else 0
+    y=b[i] if i < m else 0
 
-    total = x + y + carry
+    total=x + y + carry
 
     result.append(total % 10)
-    carry = total // 10
+    carry=total // 10
 
 if carry:
     result.append(carry)
